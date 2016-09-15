427a8b1b882e5aa269a3c5be878460dc26b33b7a
Funallingus <lachlan.gleeson@uqconnect.edu.au>
2016-09-15 12:54:56 +1000

    Add bounding box (rectangle) to objects found by the edge detection method.

--- fit_lines.m
+++ fit_lines.m
@@ -1,34 +1,30 @@
+%% Adds bounding box to all of the objects (rectangles) found
+%%by the edge detection toolbox
 %%
-% Create the Hough transform using the binary image.
 BW = E;
 
-[H,T,R] = hough(BW);
-
-%%
-% Find peaks in the Hough transform of the image.
-P  = houghpeaks(H,15,'threshold',ceil(0.5*max(H(:))));
-x = T(P(:,2)); y = R(P(:,1));
-%plot(x,y,'s','color','white');
-%%
-% Find lines and plot them.
-lines = houghlines(BW,T,R,P,'FillGap',20,'MinLength',30);
-figure, imshow(I), hold on
-max_len = 0;
-for k = 1:length(lines)
-   xy = [lines(k).point1; lines(k).point2];
-   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-
-   % Plot beginnings and ends of lines
-   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
-
-   % Determine the endpoints of the longest line segment
-   len = norm(lines(k).point1 - lines(k).point2);
-   if ( len > max_len)
-      max_len = len;
-      xy_long = xy;
-   end
+[B,L,N,A] = bwboundaries(BW);
+figure; imshow(BW); hold on;
+for k=1:length(B),
+    if(~sum(A(k,:)))
+       boundary = B{k};
+     plot(boundary(:,2),boundary(:,1),'r','LineWidth',2);hold on;
+    end
 end
 %%
-% Highlight the longest line segment by coloring it cyan.
-plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','cyan');
+%%Plot bouding boxes found via bwboudnaries
+blobMeasurements = regionprops(logical(BW), 'BoundingBox');
+numberOfBlobs = size(blobMeasurements, 1);
+
+
+rects = [];
+for k = 1 : numberOfBlobs % Loop through all blobs.
+rects = blobMeasurements(k).BoundingBox; % Get list ofpixels in current blob.
+x1 = rects(1);
+y1 = rects(2);
+x2 = x1 + rects(3);
+y2 = y1 + rects(4);
+x = [x1 x2 x2 x1 x1];
+y = [y1 y1 y2 y2 y1];
+plot(x, y, 'LineWidth', 2);
+end
