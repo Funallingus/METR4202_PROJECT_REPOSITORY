cd7a9901e7c8c162c53531f88d282f61933f947b
lucylowe <lucy@forestflying.com>
2016-09-16 11:53:36 +1000

    Add files via upload
    These aren't fully working code just things to test/ add to existing.

---
+++ depthImage.m
@@ -0,0 +1,19 @@
+%Input depth image and colour image  as well as calibration data
+%IMPORTANT:use aligned flipped image for all processing and for finding any
+%points for the pixel_real() function
+
+%http://au.mathworks.com/help/vision/ref/aligncolortodepth.html
+%http://au.mathworks.com/help/supportpkg/kinectforwindowsruntime/examples/preview-color-and-depth-streams-simultaneously-from-the-kinect-v2.html?searchHighlight=depth%20image%20kinect%20matlab
+%depthVid = videoinput('kinect',2)
+[alignedFlippedImage,flippedDepthImage] = alignColorToDepth(depthImage,colorImage,depthDevice)
+%depthDevice — Video input object
+%videoinput object | imaq.VideoDevice object
+%Video input object, specified as either a videoinput object or an imaq.VideoDevice object configured for Kinect for Windows.
+
+
+%find the point of interest u, v
+
+pixel_real(u, v, img_depth, focal_length); %RUN FUNCTION
+
+% re
+
