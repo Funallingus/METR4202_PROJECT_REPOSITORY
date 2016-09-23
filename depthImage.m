12a2ab73e2533a70ab66fb130206a4ea7f58d9c8
ash3x <ashwin.mavani.7@gmail.com>
2016-09-23 11:16:27 +1000

    Add files via upload

--- depthImage.m
+++ depthImage.m
@@ -5,7 +5,9 @@
 %http://au.mathworks.com/help/vision/ref/aligncolortodepth.html
 %http://au.mathworks.com/help/supportpkg/kinectforwindowsruntime/examples/preview-color-and-depth-streams-simultaneously-from-the-kinect-v2.html?searchHighlight=depth%20image%20kinect%20matlab
 %depthVid = videoinput('kinect',2)
-[alignedFlippedImage,flippedDepthImage] = alignColorToDepth(depthImage,colorImage,depthDevice)
+function depthImage(depthImage, colorImage, depthDevice, u, v, focal_length)
+[alignedFlippedImage,flippedDepthImage] = alignColorToDepth(depthImage,colorImage,depthDevice);
+
 %depthDevice — Video input object
 %videoinput object | imaq.VideoDevice object
 %Video input object, specified as either a videoinput object or an imaq.VideoDevice object configured for Kinect for Windows.
@@ -13,7 +15,7 @@
 
 %find the point of interest u, v
 
-pixel_real(u, v, img_depth, focal_length); %RUN FUNCTION
+pixel_real(u, v, flippedDepthImage, focal_length); %RUN FUNCTION
 
 % re
 
--- pixel_real.m
+++ pixel_real.m
@@ -1,12 +1,12 @@
 
-function [X, Y, Z] = pixel_real(u, v, img_depth, focal_length)
+function [X, Y, Z] = pixel_real(u, v, img_depth, f)
 %This function will take 1 (u, v) pixel input This should be the 
 %center of dommino or any consistant referance point 
 %must be maintained for project)
 
 %%%%% img_rgb, img_depth are the relevant rgb and depth image
 
-%focal_length - This is a output from the intrinsic parameters of 
+%f - This is a output from the intrinsic parameters of 
 %the 
 
 %Transformation extrincit matrix will be necessary to transform the 
@@ -14,10 +14,10 @@ function [X, Y, Z] = pixel_real(u, v, img_depth, focal_length)
 
 
 %find Z (depth distance) for pixel u, v
-Z = im_depth(u, v);
+Z = img_depth(u, v)
 
 %find X and Y using simplified projection model
-X = u*(Z/f);
-Y = v*(Z/f);
+X = u*(Z/f)
+Y = v*(Z/f)
 end
 
