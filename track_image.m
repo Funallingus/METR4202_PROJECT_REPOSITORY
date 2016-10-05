6229615e72880985d7ac09a4bab2f1ed8f5a139e
lucylowe <lucy@forestflying.com>
2016-10-06 00:30:19 +1000

    Add files via upload
    reduced to loop. lines between points. pixel speed.

--- track_image.m
+++ track_image.m
@@ -1,67 +1,40 @@
-function [locations] = track_image(locations, previousIm, currentIm)
+%function [locations] = track_image(locations, previousIm, currentIm)
 
 % close all;
 % previousIm = rgb2gray(imread('data_3/sequence_4.jpg'));%4
 % currentIm = rgb2gray(imread('data_3/sequence_7.jpg'));%7
+imagefiles = dir('Tracking sequence 3/*.jpg');      
+nfiles = length(imagefiles);    % Number of files found
+
+images = cell(1, nfiles);
+currentfilename = fullfile('Tracking sequence 3', imagefiles(1).name);
+currentIm = rgb2gray(imread(currentfilename));
+currentfilename = fullfile('Tracking sequence 3', imagefiles(2).name);
+previousIm = rgb2gray(imread(currentfilename));
 
 %%%% get new images
 %[frame, time, meta] = capture_frame(colorVid, depthVid);
 %currentIm = frame
 %
 tic;
-
-%locations = [870, 805, 155, 85; 820, 553, 115, 95; 970, 375, 60, 85; 1180, 415, 93, 70]
+%record time picture take
+%track_image([870, 805, 155, 85; 820, 553, 115, 95; 970, 375, 60, 85; 1180, 415, 93, 70], previousIm, currentIm)
+locations = [870, 805, 155, 85; 820, 553, 115, 95; 970, 375, 60, 85; 1180, 415, 93, 70];
 
 %%get image of objects that moved
 sub = imsubtract(previousIm, currentIm);
-imshow(sub); hold on;
+%imshow(sub); hold on;
 [maxY, maxX] = size(sub);
 %%search around domnino's in moved image for white -> a moved domino
 %%update location coordinates
-for i = 1:length(locations)
-    %%update these for a cell instead of 2D matrix
-    x = locations(i, 1);
-    y = locations(i, 2);
-    width = locations(i, 3);
-    height = locations(i, 4);
-    extraX = width/3;
-    extraY = height/3;
-    %%    coord of far left corner of search box
-    rangeX = x+extraX*2+width;
-    rangeY = extraY*2+height+y;
-    if rangeY > maxY
-        rangeY = maxY;
-    end
-    if rangeY < 0
-        rangeY = 0;
-    end
-    if rangeX > maxX
-        rangeX = maxX;
-    end
-    if rangeX < 0
-        rangeX = 0;
-    end
-    range = sub((y-extraY):(rangeY),(x-extraX):(rangeX),:);
-    range = im2bw(range);
-    [coordX1, coordY1] = track_domino(range);
-    %[coordX1, coordY1] = track_domino(extraX*3+width, extraY*3+height, range);
-    if coordX1 ~= 0
-        %% found domino
-        globalCoord = [x+coordX1-extraX, y+coordY1-extraY]
-        locations(i, 1) = globalCoord(1);
-        locations(i, 2) = globalCoord(2);
-    end
- end
-
-locations;
-
-
 
 %% scene is the image to view with pixel axis
-scene = imabsdiff(previousIm, currentIm);
+scene = currentIm; % imabsdiff(previousIm, currentIm);
 
 %% Get and display 200 frames from vi devices
-figure('Position', [100, 100, 1000, 700]); %sets the figure window size for all im
+fig = figure(30);
+fig.Position = [100, 100, 1000, 700];
+%figure('Position', [100, 100, 1000, 700]); %sets the figure window size for all im
 
 
 % There are many ways to plot an image
@@ -89,43 +62,58 @@ axis image;
 
 set(cim, 'cdata', scene);
 
-hold on;
-place = scatter(870+(locations(1,3)/2), 805+(locations(1,4)/2), 'filled');
-hold off;
-drawnow;
-
-hold on;
-place = scatter(locations(1, 1), locations(1, 2), 'filled');
-hold off;
-drawnow;
-
-hold on;
-place = scatter(820+(locations(2,3)/2), 553+(locations(2,4)/2), 'filled');
-hold off;
-drawnow;
-
-hold on;
-place = scatter(locations(2, 1), locations(2, 2), 'filled');
-hold off;
-drawnow;
-
-hold on;
-place = scatter(970+(locations(3,3)/2), 375+(locations(3,4)/2), 'filled');
-hold off;
-drawnow;
-
-hold on;
-place = scatter(locations(3, 1), locations(3, 2), 'filled');
-hold off;
-drawnow;
-
-hold on;
-place = scatter(1180+(locations(4,3)/2), 415+(locations(4,4)/2), 'filled');
-hold off;
-drawnow;
-
-hold on;
-place = scatter(locations(4, 1), locations(4, 2), 'filled');
-hold off;
-drawnow;
+for i = 1:length(locations)
+    %%update these for a cell instead of 2D matrix
+    x = locations(i, 1);
+    y = locations(i, 2);
+    width = locations(i, 3);
+    height = locations(i, 4);
+    extraX = width/3;
+    extraY = height/3;
+    %%    coord of far left corner of search box
+    rangeX = x+extraX*2+width;
+    rangeY = extraY*2+height+y;
+    
+    if rangeY > maxY
+        rangeY = maxY;
+    end
+    if rangeY < 0
+        rangeY = 0;
+    end
+    if rangeX > maxX
+        rangeX = maxX;
+    end
+    if rangeX < 0
+        rangeX = 0;
+    end
+   
+    range = sub((y-extraY):(rangeY),(x-extraX):(rangeX),:);
+    range = im2bw(range);
+    [coordX1, coordY1] = track_domino(range);
+    %[coordX1, coordY1] = track_domino(extraX*3+width, extraY*3+height, range);
+    if coordX1 ~= 0
+        %% found domino
+        globalCoord = [x+coordX1-extraX, y+coordY1-extraY];
+        u = locations(i, 1);
+        v = locations(i, 2);
+        hold on;
+        %last point
+        place = scatter(u + width/2, v + height/2, 'filled', 'blue');
+        %drawnow;
+        locations(i, 1) = globalCoord(1);
+        locations(i, 2) = globalCoord(2);
+        %New point
+        place = scatter(locations(i, 1)+ width/2, locations(i, 2)+ height/2, 'filled', 'red');
+        %drawnow;
+        line([u + width/2, locations(i, 1)+width/2], [v + height/2, locations(i, 2)+ height/2]);
+        drawnow;
+        hold off;
+        
+        %speed calc
+        distance = sqrt((u - locations(i, 1))^2 + (v - locations(i, 1))^2); %change in difference
+        time = toc;
+        speed = distance/time;
+        fprintf('Speed: %g pixels/s, distance: %g \n', speed, distance);
+    end
 end
+
