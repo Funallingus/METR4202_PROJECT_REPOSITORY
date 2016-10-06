62226fb13411831bee6b8eb9d6e0c57f780c0edf
jzpiggyfish <jzhan24@gmail.com>
2016-10-06 14:15:58 +1000

    Add files via upload

--- getWorldCoords.m
+++ getWorldCoords.m
@@ -23,20 +23,21 @@ F_depth = 365.7; % mms
 % Z_from_depth = depthIm(pixelCoords(1), pixelCoords(2))
 
 
-% Z coordinate
-z = int64(depthIm(pixelCoords(1), pixelCoords(2)));
+% Z DEPTH
+Z = int64(depthIm(pixelCoords(1), pixelCoords(2)));
 
 pixelCoords
 
-pixelCoords1 = int64(pixelCoords(1));
-pixelCoords2 = int64(pixelCoords(2));
+pixelCoords1 = int64(pixelCoords(1) * Z);
+pixelCoords2 = int64(pixelCoords(2) * Z);
 camMat14 = int64(camMatrix(1,4));
 camMat13 = int64(camMatrix(1,3));
 camMat24 = int64(camMatrix(2,4));
 camMat23 = int64(camMatrix(2,3));
 
-rhs1 = int64(pixelCoords1 - camMatrix(1,4) - camMatrix(1,3)*z);
-rhs2 = int64(pixelCoords2 - camMatrix(2,4) - camMatrix(2,3)*z);
+% LET Z EQUAL 1
+rhs1 = (pixelCoords1 - camMatrix(1,4) - camMatrix(1,3)*1)
+rhs2 = (pixelCoords2 - camMatrix(2,4) - camMatrix(2,3)*1)
 
 syms x y
 
@@ -46,17 +47,19 @@ eqn1 = camMatrix(1,1)*x + camMatrix(1,2)*y  == rhs1;
     
 eqn2 = camMatrix(2,1)*x + camMatrix(2,2)*y  == rhs2;
 
-eqn3 = camMatrix(3,1)*x + camMatrix(3,2)*y == 1;
+% eqn3 = camMatrix(3,1)*x + camMatrix(3,2)*y == 1;
 
 sol = solve([eqn1, eqn2], [x, y]);
 
 worldCoords(1) = round(sol.x);
 worldCoords(2) = round(sol.y);
+worldCoords(3) = 1;
+% worldCoords = worldCoords .* Z;
 % Pin hole model
 % worldCoords(1) = (pixelCoords(1) / f) * z;
 % worldCoords(2) = (pixelCoords(2) / f) * z;
 % x y coordinates from depth camera
 
-worldCoords(3) = z; %%THIS IS WHAT WE ARE GOING TO NEED TO FIX. 
+% worldCoords(3) = z; %%THIS IS WHAT WE ARE GOING TO NEED TO FIX. 
 
 end
