9be1c35df854f0b575f9e87ace6b3903fc35da6e
jzpiggyfish <jzhan24@gmail.com>
2016-10-13 15:51:13 +1000

    Add files via upload

---
+++ lab3_init.m
@@ -0,0 +1,15 @@
+%% Lab 3 init
+loadlibrary('dynamixel', 'dynamixel.h')
+
+%% Display Functions
+libfunctions('dynamixel')
+DEFAULT_BAUDNUM = 1;
+DEFAULT_PORTNUM = 3;
+calllib('dynamixel', 'dxl_initialize', DEFAULT_PORTNUM, DEFAULT_BAUDNUM)
+calllib('dynamixel', 'dxl_initialize', DEFAULT_PORTNUM, DEFAULT_BAUDNUM);
+
+
+%% Example code to move the motor
+%% 30 = move to end position
+%% calllib('dynamixel', 'dxl_write_word', motorID, Function, Magnitude of movement)
+calllib('dynamixel', 'dxl_write_word', 3, 30, 900)
